148acdf059933cec3dcc26b28c64072060a21b7f
Popescu Daniel <same.dan@gmail.com>
2026-02-13 19:55:09 +0100

    a lot of comments to get the backend working

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,12 +1,12 @@
 import "./list.css";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
-import { useLocation } from "react-router-dom";
-import { useState } from "react";
+// import { useLocation } from "react-router-dom";
+// import { useState } from "react";
 // import { format } from "date-fns";
 // import { DateRange } from "react-date-range";
 // import SearchItem from "../../components/searchItem/SearchItem";
-import useFetch from "./../../hooks/useFetch";
+// import useFetch from "./../../hooks/useFetch";
 
 const List = () => {
   // const location = useLocation();
@@ -21,7 +21,7 @@ const List = () => {
   //   `/hotels?city=${destination}&min=${min || 0}&max=${max || 999}`,
   // );
 
-  console.log(data);
+  // console.log(data);
 
   // const handleClick = () => {
   //   reFetch();
