4793b001afbdad03fc1b9355e2f7623306eb8d70
Popescu Daniel <same.dan@gmail.com>
2026-02-13 19:49:39 +0100

    display nothing

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -3,35 +3,35 @@ import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
-import { format } from "date-fns";
-import { DateRange } from "react-date-range";
-import SearchItem from "../../components/searchItem/SearchItem";
+// import { format } from "date-fns";
+// import { DateRange } from "react-date-range";
+// import SearchItem from "../../components/searchItem/SearchItem";
 import useFetch from "./../../hooks/useFetch";
 
 const List = () => {
-  const location = useLocation();
-  // const [destination, setDestination] = useState(location.state.destination);
-  const [dates, setDates] = useState(location.state.dates);
-  const [openDate, setOpenDate] = useState(false);
-  // const [options, setOptions] = useState(location.state.options);
-  const [min, setMin] = useState(undefined);
-  const [max, setMax] = useState(undefined);
+  // const location = useLocation();
+  // // const [destination, setDestination] = useState(location.state.destination);
+  // const [dates, setDates] = useState(location.state.dates);
+  // const [openDate, setOpenDate] = useState(false);
+  // // const [options, setOptions] = useState(location.state.options);
+  // const [min, setMin] = useState(undefined);
+  // const [max, setMax] = useState(undefined);
 
-  const { data, loading, error, reFetch } = useFetch(
-    `/hotels?city=${destination}&min=${min || 0}&max=${max || 999}`,
-  );
+  // const { data, loading, error, reFetch } = useFetch(
+  //   `/hotels?city=${destination}&min=${min || 0}&max=${max || 999}`,
+  // );
 
   console.log(data);
 
-  const handleClick = () => {
-    reFetch();
-  };
+  // const handleClick = () => {
+  //   reFetch();
+  // };
 
   return (
     <div>
       <Navbar />
       <Header type="list" />
-      <div className="listContainer">
+      {/* <div className="listContainer">
         <div className="listWrapper">
           <div className="listSearch">
             <h1 className="lsTitle">Search</h1>
@@ -115,7 +115,7 @@ const List = () => {
             )}
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
